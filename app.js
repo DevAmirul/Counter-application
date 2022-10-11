5ce8d6fd9b2fe9a501fa4300213de072dcfe6fd9
Amirul islam <dev.amirul@gmail.com>
2022-10-11 20:39:53 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+window.onload = function () {
+   main();
+};
+
+let result = 0;
+
+function main() {
+   const output = document.getElementById("output");
+   const incrementBtn = document.getElementById("increment-btn");
+   const decrementBtn = document.getElementById("decrement-btn");
+   const incrementInp = document.getElementById("increment-inp");
+   const decrementInp = document.getElementById("decrement-inp");
+   const resetBtn = document.getElementById("reset-btn");
+
+   displayResult(output);
+
+   incrementBtn.addEventListener("click", () => {
+      const incrementVal = Number(incrementInp.value);
+      result += incrementVal;
+      displayResult(output);
+   });
+   decrementBtn.addEventListener("click", () => {
+      const decrementVal = Number(decrementInp.value);
+      result -= decrementVal;
+      displayResult(output);
+   });
+
+   resetBtn.addEventListener("click", () => {
+      result = 0;
+      displayResult(output);
+      incrementInp.value = "";
+      decrementInp.value = "";
+   });
+
+   incrementInp.addEventListener("keyup", () => {
+      if (incrementInp.value < 0) {
+         incrementInp.value = Math.abs(incrementInp.value);
+      }
+   });
+
+   decrementInp.addEventListener("keyup", () => {
+      if (decrementInp.value < 0) {
+         decrementInp.value = Math.abs(decrementInp.value);
+      }
+   });
+}
+
+function displayResult(output) {
+   if (result < 0) {
+      result = 0;
+   }
+   if (result < 10) {
+      result = 0;
+   }
+   output.innerText = result;
+}
